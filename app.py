3cab62a27174032eb29c26e9c3cbd44813881811
LorenaGullone <lorenagullone03@gmail.com>
2023-11-26 15:30:03 +0100

    Add some functions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
@@ -6,14 +6,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost/logindemo'
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
+
 @app.route("/")
-def indec():
-    return "ciao"
+def home():
+    return render_template('index.html')
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -25,13 +28,12 @@ def login():
     username = data['username']
     password = data['password']
 
-    # Execute a raw SQL query to check the username and password
+    # Utilizzo di una query SQL grezza per verificare lo username e la password
     sql = f"SELECT * FROM user WHERE username = '{username}' AND password = '{password}'"
 
     conn = db.engine.connect()
 
-    
-    # Execute the vulnerable SQL query concatenating user-provided input.
+    # Esecuzione della query SQL vulnerabile concatenando l'input fornito dall'utente
     res = conn.execute(text(sql))
     names = [row[0] for row in res]
     print(names)
@@ -40,6 +42,60 @@ def login():
         return jsonify({"message": "Login successful"})
 
     return jsonify({"message": "Login failed"}), 401
+
+
+@app.route('/secure_login', methods=['POST'])
+def secure_login():
+    data = request.get_json()
+
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({"message": "Dati di richiesta non validi"}), 400
+
+    username = data['username']
+    password = data['password']
+
+    # Utilizzo di una query parametrizzata per evitare SQL injection
+    sql = text("SELECT * FROM user WHERE username = :username AND password = :password")
+
+    conn = db.engine.connect()
+
+    # Esecuzione della query utilizzando i parametri
+    res = conn.execute(sql, {"username": username, "password": password})
+    names = [row[0] for row in res]
+    print(names)
+
+    # Utilizzo di una query parametrizzata con SQLAlchemy
+    # user = User.query.filter_by(username=username, password=password).first()
+
+    if names:
+        return jsonify({"message": "Accesso riuscito"})
+
+    return jsonify({"message": "Accesso fallito"}), 401
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({"message": "Dati di richiesta non validi"}), 400
+
+    username = data['username']
+    password = data['password']
+
+    # Verifica se lo username è già in uso
+    existing_user = User.query.filter_by(username=username).first()
+
+    if existing_user:
+        return jsonify({"message": "Lo username è già in uso"}), 400
+
+    # Creazione di un nuovo utente da aggiungere al database
+    new_user = User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "Registrazione riuscita"})
+
 
 if __name__ == '__main__':
     app.run(debug=False)
