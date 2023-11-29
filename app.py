4f491a2d3396dbd2a1db83da1cea582f4a58f6b5
LorenaGullone <lorenagullone03@gmail.com>
2023-11-29 19:09:46 +0100

    Correzione 2

--- app.py
+++ app.py
@@ -19,34 +19,30 @@ def home():
 
 
 
-@app.route('/secure_login', methods=['POST'])
-def secure_login():
+@app.route('/login', methods=['POST'])
+def login():
     data = request.get_json()
 
     if not data or 'username' not in data or 'password' not in data:
-        return jsonify({"message": "Dati di richiesta non validi"}), 400
+        return jsonify({"message": "Invalid request data"}), 400
 
     username = data['username']
     password = data['password']
 
-    # Utilizzo di una query parametrizzata per evitare SQL injection
-    sql = text("SELECT * FROM user WHERE username = :username AND password = :password")
+    # Utilizzo di una query SQL grezza per verificare lo username e la password
+    sql = f"SELECT * FROM user WHERE username = '{username}' AND password = '{password}'"
 
     conn = db.engine.connect()
 
-    # Esecuzione della query utilizzando i parametri
-    res = conn.execute(sql, {"username": username, "password": password})
+    # Esecuzione della query SQL vulnerabile concatenando l'input fornito dall'utente
+    res = conn.execute(text(sql))
     names = [row[0] for row in res]
     print(names)
 
-    # Utilizzo di una query parametrizzata con SQLAlchemy
-    # user = User.query.filter_by(username=username, password=password).first()
-
     if names:
-        return jsonify({"message": "Accesso riuscito"})
+        return jsonify({"message": "Login successful"})
 
-    return jsonify({"message": "Accesso fallito"}), 401
-
+    return jsonify({"message": "Login failed"}), 401
 
 @app.route('/register', methods=['POST'])
 def register():
